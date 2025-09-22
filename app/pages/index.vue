dd3bc80a49b5552a9186e9d37250bb172ee9947c
Tomás Moroño <tomas.morono@siweb.es>
2025-09-22 18:03:35 +0200

    Initial commit

---
+++ app/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Hello World</h1>
+  </div>
+</template>
+
+<!--
+Notas acerca de Nuxt.js:
+- Para los links no se usan <a> sino <NuxtLink>
+- Los componentes deben llevar 2 palabras para diferenciarlos de los elementos HTML
+
+
+-->
