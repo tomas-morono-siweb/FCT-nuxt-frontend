d4d8a7b0a7c04ed6494f08836baa63ed8582e0f1
Tomás Moroño <tomas.morono@siweb.es>
2025-09-23 17:21:32 +0200

    Fix configuración de Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./app/**/*.{js,vue,ts}",
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.{js,vue,ts}",
+    "./pages/**/*.{js,vue,ts}",
+    "./plugins/**/*.{js,vue,ts}",
+    "./app.vue",
+    "./error.vue"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          50: '#eef6ff', 100: '#d9ecff', 200: '#bfe0ff', 300: '#93caff',
+          400: '#5dafff', 500: '#338dff', 600: '#1f6fff', 700: '#1b59e6',
+          800: '#1a49b3', 900: '#183e8f'
+        }
+      }
+    }
+  },
+  plugins: [],
+}
