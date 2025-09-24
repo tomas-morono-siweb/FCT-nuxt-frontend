47dd2d4805fd44fb4dc933a6e6c4b753bf91e731
Tomás Moroño <tomas.morono@siweb.es>
2025-09-24 11:21:48 +0200

    Movidas las notas personales del index al readme

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -111,15 +111,3 @@
     />
   </div>
 </template>
-
-<!--
-Notas acerca de Nuxt.js:
-- Para los links no se usan <a> sino <NuxtLink>
-- Los componentes deben llevar 2 palabras para diferenciarlos de los elementos HTML
-- Los componentes se pueden dividir en carpetas dentro de la carpeta components pero luego
-  deben usarse con el nombre de la carpeta como prefijo: <NavBar /> -> <CarpetaNavBar />
-- Para compartir utilidades usaremos una carpeta /utils en el directorio en el que queramos
-  compartirlas, en caso de querer que sean accesibles globalmente, la crearemos en /shared
-- En /interfaces creamos los tipos de datos para las entidades de la aplicación.
-
--->
