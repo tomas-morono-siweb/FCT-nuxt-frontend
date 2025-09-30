4868048f5652e2356468710bfea01f133687f142
Tomás Moroño <tomas.morono@siweb.es>
2025-09-30 16:02:28 +0200

    Update configs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,11 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
+  postcss: {
+    plugins: {
+      '@tailwindcss/postcss': {},
+    },
+  },
   vite: {
     plugins: [tailwindcss()],
     css: {
